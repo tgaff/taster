b86cbcb8c4c1b21974c192884657704b2ed12995
Travis Gaff <travis@labzero.com>
2023-07-21 22:47:52 +0900

    better name check?

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   resources :participant_sessions, only: [:new, :create]
   delete 'participant_sessions', to: 'participant_sessions#destroy', as: 'logout_participant_session'
   # actually doing a tasting session
-  resources :tasting, only: [:show] do
-  end
+  resources :tasting, only: [:show]
 
   resources :samples, only: [:show], controller: 'tasting/samples' do
     # resources :flavor_ratings, only: [:update], controller: 'tasting/flavor_ratings', as: 'flavor_ratings'
