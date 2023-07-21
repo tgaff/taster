b982ae1484eb955c95d0d6a7ee53ac48448b316a
Travis Gaff <travis@labzero.com>
2023-07-21 19:40:31 +0900

    partial locale setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   def participant
     return nil unless cookies.encrypted[:participant_id]
